cac84c23a0cfa4587b09a64bcf49c2db0b85ee01
Yuliya Zozulya <djulia@ukr.net>
2022-11-11 23:41:19 +0200

    adds abort controller on home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,42 +1,59 @@
 import { useState, useEffect } from "react"
-import { makeTrendingMoviesApiRequest } from "../../services/api"
+// import { makeTrendingMoviesApiRequest } from "../../services/api"
 import { TrendingMovies} from '../../components/TrendingMovies/TrendingMovies'
 import { Spinner } from "../../components/Spiner/Spiner"
 import { Title } from "./Home.styled"
+import axios from "axios"
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/'
 
 const Home = () => {
-const [movies, setMovies] = useState([])
-const [isLoading, setIsLoading] = useState(false)
+  const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  
+  useEffect(() => {
+    const controller = new AbortController()
 
-useEffect(() => {
-  const onRequestHandler = async () => {
-    try {
-      setIsLoading(true)
-      const dataMovies = await makeTrendingMoviesApiRequest()
-      const getmoviesInfo = await dataMovies.map(({title, name, id, poster_path, vote_average}) => {return {title, name, id, poster_path, vote_average}})
-      setMovies(getmoviesInfo)
-      setIsLoading(false)
-    } catch (error) {
-      console.log(error.message)
+    const onRequestHandler = async () => {
+
+    const makeTrendingMoviesApiRequest = async () => {
+      const response = await axios.get(`trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`, {signal: controller.signal})
+      return await response.data.results
+      }
+
+      try {
+        setIsLoading(true)
+        const dataMovies = await makeTrendingMoviesApiRequest()
+        const getmoviesInfo = await dataMovies.map(({title, name, id, poster_path, vote_average}) => {return {title, name, id, poster_path, vote_average}})
+        setMovies(getmoviesInfo)
+        setIsLoading(false)
+      } catch (error) {
+        console.log(error.message)
+      }
     }
+    onRequestHandler()
+
+    return () => {
+      controller.abort()
+    }
+  
+    }, []) 
+
+       return (
+        <>
+        <main>
+        {isLoading && <Spinner />}
+        {movies.length !== 0 &&
+        ( <section>
+          <Title>Trending today</Title>
+          <TrendingMovies getMovies={movies}/>
+            </section>
+          )}
+          </main>
+          </>
+      )
   }
-  onRequestHandler()
-
-  }, []) 
-     return (
-      <>
-      <main>
-      {isLoading && <Spinner />}
-      {movies.length !== 0 &&
-      ( <section>
-        <Title>Trending today</Title>
-        <TrendingMovies getMovies={movies}/>
-          </section>
-        )}
-        </main>
-        </>
-    )
-}
-
-export default Home
-
+  
+  export default Home
+  
+  
